fe97d5adfcc14f7d723b94a287cc231b39a0e90a
Chris <chrisbanning510@gmail.com>
2020-11-04 14:56:01 -0500

    put payment screen account types in stack view for conditional hiding

--- TakeMyMoney/SecondaryViewController.swift
+++ TakeMyMoney/SecondaryViewController.swift
@@ -7,16 +7,35 @@
 
 import UIKit
 
-class SecondaryViewController: UIViewController {
+enum PaymentType {
+    case credit
+    case paypal
+}
 
-    @IBOutlet weak var PaymentInfoView: UIView!
+class SecondaryViewController: UIViewController {
+    
+    var paymentType = PaymentType.paypal
+
+    @IBOutlet weak var CreditInfoView: UIView!
+    @IBOutlet weak var PayPalInfoView: UIView!
     @IBOutlet weak var PayButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        PaymentInfoView.layer.cornerRadius = 8
+        CreditInfoView.layer.cornerRadius = 8
+        PayPalInfoView.layer.cornerRadius = 8
         PayButton.layer.cornerRadius = 26
+        if paymentType == PaymentType.credit {
+            PayPalInfoView.isHidden = true
+        } else {
+            CreditInfoView.isHidden = true
+        }
+    }
+    
+    @IBAction func PayButtonPressed(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+        CreditInfoView.isHidden = true
     }
     
 
