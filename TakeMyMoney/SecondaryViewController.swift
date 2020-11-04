5485841e199948a830f8eed5a2cf774023773a65
Chris <chrisbanning510@gmail.com>
2020-11-04 12:39:17 -0500

    complete UIn for Payment Informartion Screen

--- TakeMyMoney/SecondaryViewController.swift
+++ TakeMyMoney/SecondaryViewController.swift
@@ -10,11 +10,13 @@ import UIKit
 class SecondaryViewController: UIViewController {
 
     @IBOutlet weak var PaymentInfoView: UIView!
+    @IBOutlet weak var PayButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         PaymentInfoView.layer.cornerRadius = 8
+        PayButton.layer.cornerRadius = 26
     }
     
 
