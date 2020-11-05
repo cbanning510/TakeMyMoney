253c9d63cb0b62edfaa5997e6d1bbedb31ee300b
Chris <chrisbanning510@gmail.com>
2020-11-05 08:08:23 -0500

    add datepicker and cvv with formatting

--- TakeMyMoney/MainViewController.swift
+++ TakeMyMoney/MainViewController.swift
@@ -11,38 +11,71 @@ class MainViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet weak var CreditButton: UIButton!
     @IBOutlet var PayPalButton: UIButton!
-    
     @IBOutlet weak var ProceedConfirmButton: UIButton!
     @IBOutlet weak var CreditView: UIView!
     @IBOutlet weak var PayPalView: UIView!
     @IBOutlet weak var cardNumberTextInput: UITextField!
+    @IBOutlet weak var datePickerTextField: UITextField!
+    @IBOutlet weak var CVVTextField: UITextField!
+    
+    var datePicker: UIDatePicker?
     
     var temp = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureUI()
+    }
+    
+    func configureUI() {
         cardNumberTextInput.delegate = self
-        
-       self.cardNumberTextInput.addTarget(self, action: #selector(didChangeText(textField:)), for: .editingChanged)
-        
+        CVVTextField.delegate = self
+        self.cardNumberTextInput.addTarget(self, action: #selector(didChangeText(textField:)), for: .editingChanged)
+        configureButtonUI()
+        configureDatePicker()
+        PayPalView.isHidden = true
+        CreditButton.isEnabled = false
+        PayPalButton.isEnabled = true
+        CreditButton.alpha = 0.4
+        cardNumberTextInput.setLeftPaddingPoints(104)
+    }
+    
+    func configureButtonUI () {
         PayPalButton.imageView?.contentMode = .scaleAspectFit
         PayPalButton.layer.cornerRadius = 10
         CreditButton.imageView?.contentMode = .scaleAspectFit
         CreditButton.layer.cornerRadius = 10
         ProceedConfirmButton.layer.cornerRadius = 26
-        PayPalView.isHidden = true
-        CreditButton.isEnabled = false
-        PayPalButton.isEnabled = true
-        CreditButton.alpha = 0.4
-        cardNumberTextInput.setLeftPaddingPoints(104
-        )
+    }
+    
+    func configureDatePicker() {
+        datePicker = UIDatePicker()
+        datePicker?.preferredDatePickerStyle = .wheels
+        datePicker?.datePickerMode = .date
+        datePicker?.addTarget(self, action: #selector(dateChanged(datePicker:)), for: .valueChanged)
+        datePickerTextField.inputView = datePicker
+    }
+    
+    @objc func dateChanged(datePicker:UIDatePicker) {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MM/yyyy"
+        datePickerTextField.text = dateFormatter.string(from: datePicker.date)
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(viewTapped(gestureRecognizer:)))
+        view.addGestureRecognizer(tapGesture)
+    }
+    
+    @objc func viewTapped(gestureRecognizer: UIGestureRecognizer) {
+        view.endEditing(true)
+    }
+    
+    @objc func onClickDoneButton() {
+        self.view.endEditing(true)
     }
     
     @objc func didChangeText(textField:UITextField) {
         textField.text = self.modifyCreditCardString(creditCardString: textField.text!)
     }
     
-
     func modifyCreditCardString(creditCardString : String) -> String {
         let trimmedString = creditCardString.components(separatedBy: .whitespaces).joined()
         let arrOfCharacters = Array(trimmedString)
@@ -60,7 +93,7 @@ class MainViewController: UIViewController, UITextFieldDelegate {
         }
         return resultString
     }
-
+    
     @IBAction func PayPalButtonPressed(_ sender: UIButton) {
         CreditView.isHidden = true
         PayPalView.isHidden = false
@@ -71,8 +104,10 @@ class MainViewController: UIViewController, UITextFieldDelegate {
     }
     
     @IBAction func ProceedBtnPressed(_ sender: UIButton) {
+        print("proceeding to confirm page!")
     }
-    @IBAction func CreidtButtonPressed(_ sender: UIButton) {
+    
+    @IBAction func CreditButtonPressed(_ sender: UIButton) {
         CreditView.isHidden = false
         PayPalView.isHidden = true
         CreditButton.isEnabled = false
@@ -85,25 +120,55 @@ class MainViewController: UIViewController, UITextFieldDelegate {
         let allowedCharacters = "+1234567890"
         let allowedCharacterSet = CharacterSet(charactersIn: allowedCharacters)
         let typedCharacterSet = CharacterSet(charactersIn: string)
-        
-        let MAX_LENGTH = 19
+        let MAX_LENGTH = textField == cardNumberTextInput ? 19 : 3
         let updatedString = (textField.text! as NSString).replacingCharacters(in: range, with: string)
-
         return allowedCharacterSet.isSuperset(of: typedCharacterSet) && updatedString.count <= MAX_LENGTH
-
     }
 }
 
-extension UITextField {
+extension UITextField{
+    
+    @IBInspectable var doneAccessory: Bool{
+        get{
+            return self.doneAccessory
+        }
+        set (hasDone) {
+            if hasDone{
+                addDoneButtonOnKeyboard()
+            }
+        }
+    }
+    
+    func addDoneButtonOnKeyboard() {
+        let doneToolbar: UIToolbar = UIToolbar(frame: CGRect.init(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 50))
+        doneToolbar.barStyle = .default
+        
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let done: UIBarButtonItem = UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(self.doneButtonAction))
+        
+        let items = [flexSpace, done]
+        doneToolbar.items = items
+        doneToolbar.sizeToFit()
+        
+        self.inputAccessoryView = doneToolbar
+    }
+    
+    @objc func doneButtonAction() {
+        self.resignFirstResponder()
+    }
+    
     func setLeftPaddingPoints(_ amount:CGFloat){
         let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: amount, height: self.frame.size.height))
         self.leftView = paddingView
         self.leftViewMode = .always
     }
+    
     func setRightPaddingPoints(_ amount:CGFloat) {
         let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: amount, height: self.frame.size.height))
         self.rightView = paddingView
         self.rightViewMode = .always
-    }
+    }    
 }
+
+
 
